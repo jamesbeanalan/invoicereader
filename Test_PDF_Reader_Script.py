67808fbdfe9427dda6f7a7f2912d18d0024b3a3d
jamesbeanalan <e0960068@u.nus.edu>
2024-07-24 09:28:58 +0800

    Delete Test_PDF_Reader_Script.py

--- Test_PDF_Reader_Script.py
+++
@@ -1,9 +0,0 @@
-directory = r"C:\Users\alanbean\Downloads\Additional\PDF_compiler\210624"
-
-from PDF_reader_script import *
-
-# files_in_folder(directory)
-
-# list_folders(directory)
-
-list_all_files_in_all_folders(directory)
